e54e2eb169b128c87def0bbebf3e4d7f90dec5bf
Roy Tseng <stu121645162002@gmail.com>
2022-12-24 23:33:06 +0800

    create Operation

--- src/Objects.jl
+++ src/Objects.jl
@@ -1,6 +1,23 @@
 module Objects
 
-export 
-    Order
-    include("./Order.jl")
+export Order, Operation
+    """
+    Job工作
+    Job 可以是工單、行程、任務、工作 任何想要排程的物件。
+    其元素必備該工作的id, info (product_id, 截止日期) 
+    """
+    mutable struct Order 
+        id::String #工作的id
+        info::Dict{Any} #交期 #產品號
+    end
+
+    """
+    Operation工序
+    Operation 可以是工作的子程序
+    其元素必備該工序的id, info (product_id, 截止日期) 
+    """
+    mutable struct Operation 
+        id::String #工作的id
+        info::Dict{Any} #交期 #產品號
+    end
 end # module
